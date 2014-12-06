e15daa075b5569eedc7ba7afd97a27df92bf8de2
Neil Goldman <neilgoldman305@gmail.com>
2014-12-06 15:52:44 -0500

    Added Player

--- too_many_cooks/player.py
+++ too_many_cooks/player.py
@@ -1,1 +1,16 @@
-__author__ = 'neil@everymundo.com'
+import os
+import pygame
+from too_many_cooks import GlobalVars
+
+
+class Player(object):
+    def __init__(self):
+        super().__init__()
+        self.scale = GlobalVars.scale
+        image = pygame.image.load(os.path.join('sprites', 'player.png'))
+        self.image = pygame.transform.scale(image,
+                                            int(image.get_width() * self.scale),
+                                            int(image.get_height() * self.scale))
+        self.current_tile = None
+        self.vel_x = 0.0
+        self.vel_y = 0.0
