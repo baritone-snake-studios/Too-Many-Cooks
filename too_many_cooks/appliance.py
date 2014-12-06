d6206cad0ec0457608a94b288550bc078facae1a
pablomorera <mopablo50@yahoo.com>
2014-12-06 15:49:25 -0500

    Added Appliances

--- too_many_cooks/appliance.py
+++ too_many_cooks/appliance.py
@@ -1,1 +1,56 @@
 __author__ = 'neil@everymundo.com'
+
+
+class Appliance(Tile):
+    def __init__(self, image_path):
+        super().__init__(image=image_path, is_colliding=True)
+
+    def use(self):
+        pass
+
+
+class Grill(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class Fryer(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class ChoppingBlock(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class Oven(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class CounterTop(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class Barrel(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class Fridge(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class VegieFreezer(Appliance):
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
+class DessertFreezer(Appliance)
+    def __init__(self, image_path):
+        super().__init__(image_path)
+
+
