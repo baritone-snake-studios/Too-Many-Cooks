f541f34d079d0a1a835e369cfa4a85876f9ee857
Neil Goldman <neilgoldman305@gmail.com>
2014-12-07 19:53:29 -0500

    Can combine ingredients to make recipes

--- too_many_cooks/recipe.py
+++ too_many_cooks/recipe.py
@@ -1,12 +1,20 @@
 
 class Recipe(object):
+    required_ingredients_names = []
+
     @classmethod
     def can_make(cls, ingredients):
-        pass
+        for ingr in ingredients:
+            if ingr.name not in cls.required_ingredients_names:
+                return False
+        return True
 
 
 class BurgerRecipe(Recipe):
+    required_ingredients_names = ['Cooked Patty', 'Burger Buns', 'Lettuce', 'Tomato']
     pass
 
+
 class FryRecipe(Recipe):
+    required_ingredients_names = ['French Fries']
     pass
